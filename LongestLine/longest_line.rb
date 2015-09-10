4eba013e1f5a171c9c5076192bda51fed2fb1300
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-09-10 13:49:32 +0300

    longest line in a file with a limit

---
+++ LongestLine/longest_line.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'pp'
+
+result = []
+myFile = File.open(ARGV[0])
+limit = myFile.gets.to_i
+
+while (line = myFile.gets) do 
+    line.delete!("\n")
+    result << line
+end
+puts result.sort_by(&:length).reverse.shift(limit)
+
